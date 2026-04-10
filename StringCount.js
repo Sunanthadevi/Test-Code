c4731846f01dedecea931be139516deddda1eef4
Sunanthadevi <sunanthadevi.vm@gmail.com>
2026-04-11 00:33:40 +0530

    Delete StringCount.js

--- StringCount.js
+++
@@ -1,9 +0,0 @@
-let count=0
-let inputArray=[2,1,2,3,6,8,2]
-
-for(let i=0;i<=inputArray.length-1;i++){
-    if(inputArray[i]=='2'){
-        count=count+1
-    }
-}
-console.log("The number '2' occured "+count + " times in the array" + inputArray)
